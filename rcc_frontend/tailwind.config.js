62537869ffadf710b508cde5b18abc7d434ef8ce
Kibet <mshckkibet@gmail.com>
2024-10-15 15:59:49 +0300

    added components to talwindconfig

--- rcc_frontend/tailwind.config.js
+++ rcc_frontend/tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
         },
+        'fade-in-up': {
+          '0%': { transform: 'translateY(20px)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
+        },
         'fade-in-left': {
           '0%': { transform: 'translateX(-100%)', opacity: 0 },
           '100%': { transform: 'translateX(0)', opacity: 1 },
@@ -24,7 +28,9 @@ module.exports = {
         'fade-in': 'fade-in 2s ease-in-out',
         'fade-in-left': 'fade-in-left 1.5s ease-out',
         'bounce-in': 'bounce-in 2.5s ease-out',
+        'fade-in-up': 'fade-in-up 1.5s ease-out',
       },
+      
     },
   },
   plugins: [],
