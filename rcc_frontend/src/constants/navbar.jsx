a4f53f8774a8d6aaa20df2ca8f1950cc0dd5fe1e
Kibet <mshckkibet@gmail.com>
2024-10-15 22:47:20 +0300

    done with most final changes

--- rcc_frontend/src/constants/navbar.jsx
+++ rcc_frontend/src/constants/navbar.jsx
@@ -65,7 +65,7 @@ export default function Navbar() {
             {openDropdown === 'climateMonitoring' && (
               <div className="absolute mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50">
                 <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                  <a href="/reference-climatology" className="block px-4 py-2 text-sm text-gray-700 hover:bg-green-100" role="menuitem">
+                  <a href="/reference" className="block px-4 py-2 text-sm text-gray-700 hover:bg-green-100" role="menuitem">
                     Reference Climatology
                   </a>
                 </div>
--- rcc_frontend/src/pages/reference.jsx
+++ rcc_frontend/src/pages/reference.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import skill from '../public/skills-diagnostics.png';
 
-
 const Reference = () => {
   const [selectedMonth, setSelectedMonth] = useState('Jan');
   const [selectedSeason, setSelectedSeason] = useState('Annual');
@@ -29,49 +28,63 @@ const Reference = () => {
 
   return (
     <div className='overflow-hidden'>    
-    <div
+      <div
         className="relative w-full bg-cover bg-center text-white"
-        style={{ backgroundImage: `url(${skill})`, width: '100%',
-          height: '100%',  }}
+        style={{ backgroundImage: `url(${skill})`, width: '100%', height: '100%' }}
       >
         <h2 className="text-4xl font-bold pt-60 pl-10 animate-fade-in pb-10">Skill Diagnostics</h2>
       </div>
-    <div className="flex flex-col bg-slate-100">
-      <div className="flex gap-4 justify-around my-4">
-        {/* Month Selector */}
-        <select value={selectedMonth} onChange={(e) => setSelectedMonth(e.target.value)} className="border p-2 rounded-md shadow-sm">
-          <option value="Jan">January</option>
-          <option value="Feb">February</option>
-          <option value="Mar">March</option>
-          <option value="Apr">April</option>
-          <option value="May">May</option>
-          <option value="June">June</option>
-          <option value="July">July</option>
-          <option value="Aug">August</option>
-          <option value="Sept">September</option>
-          <option value="Oct">October</option>
-          <option value="Nov">November</option>
-          <option value="Dec">December</option>
-          {/* Add other month options here */}
-        </select>
 
-        {/* Season Selector */}
-        <select value={selectedSeason} onChange={(e) => setSelectedSeason(e.target.value)} className="border p-2 rounded-md shadow-sm">
-          <option value="Annual">Annual</option>
-          <option value="JJAS">JJAS</option>
-          <option value="OND">OND</option>
-          <option value="DJF">DJF</option>
-        </select>
-      </div>
+      <div className="flex flex-col bg-slate-100">
+        <div className="flex gap-4 justify-around my-4">
+          {/* Month Selector with Label */}
+          <div className="flex flex-col">
+            <label htmlFor="month-selector" className="text-xl text-gray-700 font-semibold mx-6 my-3 hover:underline">Select Month</label>
+            <select
+              id="month-selector"
+              value={selectedMonth}
+              onChange={(e) => setSelectedMonth(e.target.value)}
+              className="border p-2 rounded-md shadow-sm"
+            >
+              <option value="Jan">January</option>
+              <option value="Feb">February</option>
+              <option value="Mar">March</option>
+              <option value="Apr">April</option>
+              <option value="May">May</option>
+              <option value="June">June</option>
+              <option value="July">July</option>
+              <option value="Aug">August</option>
+              <option value="Sept">September</option>
+              <option value="Oct">October</option>
+              <option value="Nov">November</option>
+              <option value="Dec">December</option>
+            </select>
+          </div>
 
-      <div className="flex gap-4 mx-7 mb-7">
-        {images.length > 0 && images.map((img) => (
-          <img key={img.image} src={img.image} alt={`${img.month} - ${img.season}`} className="w-1/2" />
-        ))}
+          {/* Season Selector with Label */}
+          <div className="flex flex-col">
+          <label htmlFor="season-selector" className="text-xl text-gray-700 font-semibold mx-6 my-3 hover:underline">Select Season</label>
+          <select
+              id="season-selector"
+              value={selectedSeason}
+              onChange={(e) => setSelectedSeason(e.target.value)}
+              className="border p-2 rounded-md shadow-sm"
+            >
+              <option value="Annual">Annual</option>
+              <option value="JJAS">JJAS</option>
+              <option value="OND">OND</option>
+              <option value="DJF">DJF</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="flex gap-4 mx-7 mb-7">
+          {images.length > 0 && images.map((img) => (
+            <img key={img.image} src={img.image} alt={`${img.month} - ${img.season}`} className="w-1/2" />
+          ))}
+        </div>
       </div>
     </div>
-    </div>
-
   );
 };
 
